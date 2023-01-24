69e9c295648dba15f8723d9c80d596ddf081183c
Jack Snider <choiman2021@gmail.com>
2023-01-24 19:36:44 +0900

    route

--- server.py
+++ server.py
@@ -7,7 +7,18 @@ app = Flask( __name__ )
 def index():
 
     # return type should be String
-    return 'random : <strong>' + str( random.random() ) + '</strong>'
+    return 'Welcome'
+
+@app.route( '/create' )
+def create():
+    return 'Create'
+
+# if you want to add a parameter in route
+# you have to add parameter in funtion as well
+@app.route( '/read/<id>/' )
+def read( id ):
+    print( id )
+    return 'Read ' + id
 
 # stop server : Ctrl + c
 # debug = True : when you fix the code, it reflects to the server autometically
