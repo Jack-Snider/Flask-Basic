51677cad6902b7ece21f9b9fe802252195fb35e2
Jack Snider <choiman2021@gmail.com>
2023-02-12 14:41:50 +0900

    redirect

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, request, redirect
+
 import random
 
 # create Flask object for server
 app = Flask( __name__ )
 
+nextId = 4
 
 topics = [
     { 'id':1, 'title' : 'html', 'body' : 'html is ...' },
@@ -59,17 +61,29 @@ def read( id ):
 
     return template( getContents(), f'<h2>{title}</h2>{body}' )
 
-@app.route( '/create/' )
+@app.route( '/create/', methods = [ 'GET', 'POST' ] )
 def create():
-    content = \
-    '''
-        <form action = "/create/" method = "POST">
-            <p><input type = "text" name = "title" placeholder = "title"></p>
-            <p><textarea name = "body" placeholder = "body"></textarea></p>
-            <p><input type = "submit" value = "create"></p>
-        <form>
-    '''
-    return template( getContents(), content )
+    print( f'request.method : {request.method}' )
+    if request.method == 'GET':
+        content = \
+        '''
+            <form action = "/create/" method = "POST">
+                <p><input type = "text" name = "title" placeholder = "title"></p>
+                <p><textarea name = "body" placeholder = "body"></textarea></p>
+                <p><input type = "submit" value = "create"></p>
+            <form>
+        '''
+        return template( getContents(), content )
+    elif request.method == 'POST':
+        global nextId
+        title = request.form[ 'title' ]
+        body = request.form[ 'body' ]
+        newTopic = { 'id':nextId, 'title':title, 'body':body }
+        topics.append( newTopic )
+        url = '/read/' + str( nextId ) + '/'
+        nextId = nextId +  1
+        return redirect( url )
+        
 
 
 # stop server : Ctrl + c
